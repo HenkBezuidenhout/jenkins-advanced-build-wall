5c579f8bfacd511e269b22c4fd3c1dd8fb713c90
Henk Bezuidenhout <henkbezuidenhout@gmail.com>
2015-08-25 16:15:54 +0200

    fixup: fixed some of the issues with the page not reloading information, and styling not working

--- index.js
+++ index.js
@@ -8,47 +8,59 @@ var JENKINS = require('jenkins-api');
 
 var _express = EXPRESS();
 //var _jenkinsApiUrl = "https://www.jenkins-ci.org/api/json";
-var _jenkins = JENKINS.init("http://jenkins.nodejs.org/");
+//var _jenkins = JENKINS.init("http://jenkins.nodejs.org/");
 //var _jenkins = JENKINS.init("https://www.jenkins-ci.org/");
 //var _jenkins = JENKINS.init("http://localhost:8080/");
+var _jenkins = JENKINS.init("http://nvi-prd-jen01.nvisionit.co.za:8080/");
 
 _express.use(LESS_MIDDLEWARE(__dirname + '/public', {
-	dest: __dirname + '/public',
-	enable: ['less'],
-	force: true,
-	debug: true
+    force: true,
+    debug: true
 }));
 _express.use(EXPRESS.static(__dirname + '/public'));
-_express.use('/bower_components',  EXPRESS.static(__dirname + '/bower_components'));
+_express.use('/bower_components', EXPRESS.static(__dirname + '/bower_components'));
 
-_express.get('/all_jobs', function (req, res) {	
-	_jenkins.all_jobs(function(err, data) {
-    	if (err){ return console.log(err); }	
-		var jobs = data.slice(0, 16);
-		
-		res.json(jobs);
-	});	
+var cached_all_jobs = {};
+
+_express.get('/all_jobs', function (req, res) {
+    _jenkins.all_jobs(function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            cached_all_jobs = data;
+        }
+        res.json(cached_all_jobs);
+    });
+});
+_express.get('/job_info', function (req, res) {
+    console.log(req.query);
+    _jenkins.job_info(req.query.name, function (err, data) {
+        if (err) { return console.log(err); }
+        res.json(data);
+    });
 });
 
-_express.get('/job_info', function (req, res) {	
-	console.log(req.query);
-	_jenkins.job_info(req.query.name, function(err, data) {
-    	if (err){ return console.log(err); }		
-		res.json(data);
-	});	
-});
+var cached_build_reports = [null];
 
-_express.get('/last_build_report', function (req, res) {	
-	console.log(req.query);
-	_jenkins.last_build_report(req.query.name, function(err, data) {
-    	if (err){ return console.log(err); }		
-		res.json(data);
-	});	
+_express.get('/last_build_report', function (req, res) {
+    var job_name = req.query.name;
+    console.log(job_name);
+
+    _jenkins.last_build_report(job_name, function (err, data) {
+        console.log(data);
+        if (err) {
+            console.log(err);
+        } else {
+            cached_build_reports[job_name] = data;
+            console.log(data);
+        }
+        res.json(cached_build_reports[job_name]);
+    });
 });
 
 var server = _express.listen(3000, function () {
-	var host = server.address().address;
-	var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-	console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
