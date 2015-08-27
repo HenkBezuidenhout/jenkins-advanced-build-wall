84bd54b366c85fe195af6a483a8a41b4a8089128
Henk Bezuidenhout <henkbezuidenhout@gmail.com>
2015-08-27 13:45:18 +0200

    feat: added full sonarqube integration and dropped the Jenkins API in favor of a standard rest object request.

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ var EXPRESS = require('express');
 var PATH = require('path');
 var LESS = require('less');
 var LESS_MIDDLEWARE = require('less-middleware');
-var HTTPS = require('https');
-var HTTP = require('http');
+var NODE_REST_CLIENT = require('node-rest-client').Client;
 var JENKINS = require('jenkins-api');
+var QUERY_STRING = require('querystring');
+
+var SonarQubeUrl = 'http://nvi-prd-jen01.nvisionit.co.za:9000';
+var JenkinsUrl = 'http://nvi-prd-jen01.nvisionit.co.za:8080';
+
+var DataCache = [null];
 
 var _express = EXPRESS();
-//var _jenkinsApiUrl = "https://www.jenkins-ci.org/api/json";
-var _jenkins = JENKINS.init("http://jenkins.nodejs.org/");
-//var _jenkins = JENKINS.init("https://www.jenkins-ci.org/");
-//var _jenkins = JENKINS.init("http://localhost:8080/");
+
+var _jenkins = new NODE_REST_CLIENT();
+_jenkins.registerMethod("listAllJobs", JenkinsUrl + '/api/json?depth=0', 'GET');
+_jenkins.registerMethod("getJobInfo", JenkinsUrl + '/job/${resource}/api/json?depth=5', 'GET');
+_jenkins = _jenkins.methods;
+
+var _sonarqube = new NODE_REST_CLIENT();
+_sonarqube.registerMethod("getMetrics", SonarQubeUrl + '/api/resources', 'GET');
+_sonarqube = _sonarqube.methods;
 
 _express.use(LESS_MIDDLEWARE(__dirname + '/public', {
 	dest: __dirname + '/public',
@@ -20,32 +30,102 @@ _express.use(LESS_MIDDLEWARE(__dirname + '/public', {
 }));
 
 _express.use(EXPRESS.static(__dirname + '/public'));
-_express.use('/bower_components',  EXPRESS.static(__dirname + '/bower_components'));
+_express.use('/bower_components', EXPRESS.static(__dirname + '/bower_components'));
 
-_express.get('/all_jobs', function (req, res) {	
-	_jenkins.all_jobs(function(err, data) {
-    	if (err){ return console.log(err); }	
-		var jobs = data.slice(0, 16);
-		
-		res.json(jobs);
-	});	
+_express.get('/api/list', function (req, res) {
+	_jenkins.listAllJobs({}, function (data) {
+		var jobs = data.jobs.slice(0, 16);
+
+		var list = [];
+
+		for (var job in jobs) {
+			job = jobs[job];
+			list[list.length] = job.name;
+			if (!DataCache[job.name]) {
+				DataCache[job.name] = {
+					name: job.name,
+					status: job.color
+				};
+			}
+		}
+
+		res.json(list);
+	});
 });
 
-_express.get('/job_info', function (req, res) {	
-	console.log(req.query);
-	_jenkins.job_info(req.query.name, function(err, data) {
-    	if (err){ return console.log(err); }		
-		res.json(data);
-	});	
+_express.get('/api/details', function (req, res) {
+	var job_name = req.query.name;
+	if (!DataCache[job_name]) {
+		DataCache[job_name] = {
+			name: job_name
+		};
+	}
+
+	_jenkins.getJobInfo({
+		path: {
+			resource: job_name
+		}
+	},
+		function (data) {
+			var obj = DataCache[job_name];
+			try {
+				obj.scm = data.scm.type;
+				obj.status = data.color;
+				obj.name = data.displayName;
+				obj.healthReport = data.healthReport;
+				obj.build = {
+					busy: data.lastBuild.building,
+					name: data.lastBuild.displayName,
+					progress: (data.lastBuild.building) ? data.lastBuild.executor.progress : 100,
+					result: data.lastBuild.result,
+					timestamp: data.lastBuild.timestamp,
+					culprits: data.lastBuild.culprits
+				};
+				for (var action in data.lastBuild.actions) {
+					action = data.lastBuild.actions[action];
+					if (action.url) {
+						var url = action.url.toLowerCase();
+						var server = SonarQubeUrl.toLowerCase();
+						var idx = 'index/';
+						if (url.indexOf(server) >= 0 && url.lastIndexOf(idx) > 0) {
+							var resource = url.substr(url.lastIndexOf(idx) + idx.length);
+							if (obj.sonarqube) {
+								obj.sonarqube.resource = resource;
+							} else {
+								obj.sonarqube = {
+									resource: resource
+								};
+							}
+						}
+					} else if (action.urlName && action.urlName.indexOf('testReport') === 0){
+						obj.tests = action;
+					}
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		});
+	var details = DataCache[job_name];
+	if (details.sonarqube) {
+		_sonarqube.getMetrics({
+			parameters: {
+				resource: details.sonarqube.resource,
+				metrics: 'ncloc,coverage,new_coverage,sqale_index,sqale_rating,sqale_debt_ratio'
+			},
+			headers: { "Accept": "application/json" }
+		}, function (data) {
+			var obj = DataCache[job_name];
+
+			data = data[0];
+			for (var msr in data.msr) {
+				msr = data.msr[msr];
+				obj.sonarqube[msr.key] = msr.frmt_val;
+			}
+		});
+	}
+	res.json(details);
 });
 
-_express.get('/last_build_report', function (req, res) {	
-	console.log(req.query);
-	_jenkins.last_build_report(req.query.name, function(err, data) {
-    	if (err){ return console.log(err); }		
-		res.json(data);
-	});	
-});
 
 var server = _express.listen(3000, function () {
 	var host = server.address().address;
